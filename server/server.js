39d4dc5f3bd39b16c67ac8c5f03646d0560e8d53
teresa <teresa.s.lacroix@gmail.com>
2020-05-10 18:57:17 -0400

    make server typescript

--- server/server.js
+++ server/server.js
@@ -1,49 +1,42 @@
-const express = require('express');
-const app = express();
-const multer = require('multer');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var multer = require('multer');
 // const upload = multer({ dest: __dirname + '/build/uploads/' });
-const upload = multer({ dest: '../client/public/uploads/' });
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const sha1 = require('sha1');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-const cookieParser = require('cookie-parser');
+var upload = multer({ dest: '../client/public/uploads/' });
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var sha1 = require('sha1');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
+var cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
 // Database;
-let dbo = undefined;
-const url =
-  'mongodb+srv://terandy:pw@cluster0-zo8n8.mongodb.net/test?retryWrites=true&w=majority';
-MongoClient.connect(
-  url,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, db) => {
+var dbo = undefined;
+var url = 'mongodb+srv://terandy:pw@cluster0-zo8n8.mongodb.net/test?retryWrites=true&w=majority';
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function (err, db) {
     dbo = db.db('chat-bot');
     dbo.collection('users').createIndex({ '$**': 'text' });
     if (err) {
-      console.log('ERROR', err);
+        console.log('ERROR', err);
     }
-  }
-);
-
+});
 // app.get('/*', (req, res) => {
 //   res.sendFile(__dirname + '/build/index.html');
 // });
-app.get('/', (req, res) => {
-  res.send('server is running....');
+app.get('/', function (req, res) {
+    res.send('server is running....');
 });
-
-app.post('/send-msg', upload.none(), (req, res) => {
-  console.log('send-msg', req.body.msg);
-  dbo
-    .collection('messages')
-    .insertOne({ content: req.body.msg, sender: req.body.sender }); //needs work
-  res.json({ success: true, message: 'sent' });
+app.post('/send-msg', upload.none(), function (req, res) {
+    console.log('send-msg', req.body.msg);
+    dbo
+        .collection('messages')
+        .insertOne({ content: req.body.msg, sender: req.body.sender }); //needs work
+    res.json({ success: true, message: 'sent' });
 });
-
-const { PORT = 5000, LOCAL_ADDRESS = '0.0.0.0' } = process.env;
-server.listen(PORT, LOCAL_ADDRESS, () => {
-  const address = server.address();
-  console.log('server listening at', address);
+var _a = process.env, _b = _a.PORT, PORT = _b === void 0 ? 5000 : _b, _c = _a.LOCAL_ADDRESS, LOCAL_ADDRESS = _c === void 0 ? '0.0.0.0' : _c;
+server.listen(PORT, LOCAL_ADDRESS, function () {
+    var address = server.address();
+    console.log('server listening at', address);
 });
---
+++ server/server.ts
@@ -0,0 +1,49 @@
+import express = require('express');
+const app: express.Application = express();
+const multer = require('multer');
+// const upload = multer({ dest: __dirname + '/build/uploads/' });
+const upload = multer({ dest: '../client/public/uploads/' });
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const sha1 = require('sha1');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+// Database;
+let dbo: any = undefined;
+const url =
+  'mongodb+srv://terandy:pw@cluster0-zo8n8.mongodb.net/test?retryWrites=true&w=majority';
+MongoClient.connect(
+  url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err: any, db: any) => {
+    dbo = db.db('chat-bot');
+    dbo.collection('users').createIndex({ '$**': 'text' });
+    if (err) {
+      console.log('ERROR', err);
+    }
+  }
+);
+
+// app.get('/*', (req, res) => {
+//   res.sendFile(__dirname + '/build/index.html');
+// });
+app.get('/', (req, res) => {
+  res.send('server is running....');
+});
+
+app.post('/send-msg', upload.none(), (req, res) => {
+  console.log('send-msg', req.body.msg);
+  dbo
+    .collection('messages')
+    .insertOne({ content: req.body.msg, sender: req.body.sender }); //needs work
+  res.json({ success: true, message: 'sent' });
+});
+
+const { PORT = 5000, LOCAL_ADDRESS = '0.0.0.0' } = process.env;
+server.listen(PORT, LOCAL_ADDRESS, () => {
+  const address = server.address();
+  console.log('server listening at', address);
+});
